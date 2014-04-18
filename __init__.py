d73a1f6f52b91a73ec55b9abad3846eb82676a74
Patrick Moore <patrick.moore.bu@gmail.com>
2014-04-17 23:13:39 -0400

    alignment quality reporting
    some new numpy implementations for testing

--- __init__.py
+++ __init__.py
@@ -3,6 +3,7 @@ Created on Apr 14, 2014
 
 @author: Patrick
 '''
+from numpy.ma.core import fmod
 bl_info = {
     "name": "Complex Alignment",
     "author": "Patrick Moore",
@@ -16,6 +17,7 @@ bl_info = {
 
 
 import numpy as np
+import math
 import time
 import bpy
 import blf
@@ -26,6 +28,9 @@ from bpy.types import Operator, AddonPreferences
 from bpy_extras.object_utils import AddObjectHelper, object_data_add
 from bpy_extras import view3d_utils
 from mathutils import Vector, Matrix, Quaternion
+
+# epsilon for testing whether a number is close to zero
+_EPS = numpy.finfo(float).eps * 4.0
 
 def tag_redraw_all_view3d():
     context = bpy.context
@@ -102,6 +107,182 @@ def draw_3d_text(context, font_id, text, vec):
 
         blf.position(font_id, x + 3.0, y - 4.0, 0.0)
         blf.draw(font_id, text)    
+
+
+def quaternion_matrix(quaternion):
+    """Return homogeneous rotation matrix from quaternion.
+
+    >>> M = quaternion_matrix([0.99810947, 0.06146124, 0, 0])
+    >>> numpy.allclose(M, rotation_matrix(0.123, [1, 0, 0]))
+    True
+    >>> M = quaternion_matrix([1, 0, 0, 0])
+    >>> numpy.allclose(M, numpy.identity(4))
+    True
+    >>> M = quaternion_matrix([0, 1, 0, 0])
+    >>> numpy.allclose(M, numpy.diag([1, -1, -1, 1]))
+    True
+
+    """
+    q = np.array(quaternion, dtype=np.float64, copy=True)
+    n = np.dot(q, q)
+    if n < _EPS:
+        return np.identity(4)
+    q *= math.sqrt(2.0 / n)
+    q = np.outer(q, q)
+    return np.array([
+        [1.0-q[2, 2]-q[3, 3],     q[1, 2]-q[3, 0],     q[1, 3]+q[2, 0], 0.0],
+        [    q[1, 2]+q[3, 0], 1.0-q[1, 1]-q[3, 3],     q[2, 3]-q[1, 0], 0.0],
+        [    q[1, 3]-q[2, 0],     q[2, 3]+q[1, 0], 1.0-q[1, 1]-q[2, 2], 0.0],
+        [                0.0,                 0.0,                 0.0, 1.0]])
+    
+    
+def vector_norm(data, axis=None, out=None):
+    """Return length, i.e. Euclidean norm, of ndarray along axis.
+
+    >>> v = numpy.random.random(3)
+    >>> n = vector_norm(v)
+    >>> numpy.allclose(n, numpy.linalg.norm(v))
+    True
+    >>> v = numpy.random.rand(6, 5, 3)
+    >>> n = vector_norm(v, axis=-1)
+    >>> numpy.allclose(n, numpy.sqrt(numpy.sum(v*v, axis=2)))
+    True
+    >>> n = vector_norm(v, axis=1)
+    >>> numpy.allclose(n, numpy.sqrt(numpy.sum(v*v, axis=1)))
+    True
+    >>> v = numpy.random.rand(5, 4, 3)
+    >>> n = numpy.empty((5, 3))
+    >>> vector_norm(v, axis=1, out=n)
+    >>> numpy.allclose(n, numpy.sqrt(numpy.sum(v*v, axis=1)))
+    True
+    >>> vector_norm([])
+    0.0
+    >>> vector_norm([1])
+    1.0
+
+    """
+    data = np.array(data, dtype=np.float64, copy=True)
+    if out is None:
+        if data.ndim == 1:
+            return math.sqrt(np.dot(data, data))
+        data *= data
+        out = np.atleast_1d(np.sum(data, axis=axis))
+        np.sqrt(out, out)
+        return out
+    else:
+        data *= data
+        np.sum(data, axis=axis, out=out)
+        np.sqrt(out, out)
+        
+def affine_matrix_from_points(v0, v1, shear=True, scale=True, usesvd=True):
+    """Return affine transform matrix to register two point sets.
+
+    v0 and v1 are shape (ndims, \*) arrays of at least ndims non-homogeneous
+    coordinates, where ndims is the dimensionality of the coordinate space.
+
+    If shear is False, a similarity transformation matrix is returned.
+    If also scale is False, a rigid/Euclidean transformation matrix
+    is returned.
+
+    By default the algorithm by Hartley and Zissermann [15] is used.
+    If usesvd is True, similarity and Euclidean transformation matrices
+    are calculated by minimizing the weighted sum of squared deviations
+    (RMSD) according to the algorithm by Kabsch [8].
+    Otherwise, and if ndims is 3, the quaternion based algorithm by Horn [9]
+    is used, which is slower when using this Python implementation.
+
+    The returned matrix performs rotation, translation and uniform scaling
+    (if specified).
+
+    >>> v0 = [[0, 1031, 1031, 0], [0, 0, 1600, 1600]]
+    >>> v1 = [[675, 826, 826, 677], [55, 52, 281, 277]]
+    >>> affine_matrix_from_points(v0, v1)
+    array([[   0.14549,    0.00062,  675.50008],
+           [   0.00048,    0.14094,   53.24971],
+           [   0.     ,    0.     ,    1.     ]])
+    >>> T = translation_matrix(numpy.random.random(3)-0.5)
+    >>> R = random_rotation_matrix(numpy.random.random(3))
+    >>> S = scale_matrix(random.random())
+    >>> M = concatenate_matrices(T, R, S)
+    >>> v0 = (numpy.random.rand(4, 100) - 0.5) * 20
+    >>> v0[3] = 1
+    >>> v1 = numpy.dot(M, v0)
+    >>> v0[:3] += numpy.random.normal(0, 1e-8, 300).reshape(3, -1)
+    >>> M = affine_matrix_from_points(v0[:3], v1[:3])
+    >>> numpy.allclose(v1, numpy.dot(M, v0))
+    True
+
+    More examples in superimposition_matrix()
+
+    """
+    v0 = np.array(v0, dtype=np.float64, copy=True)
+    v1 = np.array(v1, dtype=np.float64, copy=True)
+
+    ndims = v0.shape[0]
+    if ndims < 2 or v0.shape[1] < ndims or v0.shape != v1.shape:
+        raise ValueError("input arrays are of wrong shape or type")
+
+    # move centroids to origin
+    t0 = -np.mean(v0, axis=1)
+    M0 = np.identity(ndims+1)
+    M0[:ndims, ndims] = t0
+    v0 += t0.reshape(ndims, 1)
+    t1 = -np.mean(v1, axis=1)
+    M1 = np.identity(ndims+1)
+    M1[:ndims, ndims] = t1
+    v1 += t1.reshape(ndims, 1)
+
+    if shear:
+        # Affine transformation
+        A = np.concatenate((v0, v1), axis=0)
+        u, s, vh = np.linalg.svd(A.T)
+        vh = vh[:ndims].T
+        B = vh[:ndims]
+        C = vh[ndims:2*ndims]
+        t = np.dot(C, np.linalg.pinv(B))
+        t = np.concatenate((t, np.zeros((ndims, 1))), axis=1)
+        M = np.vstack((t, ((0.0,)*ndims) + (1.0,)))
+    elif usesvd or ndims != 3:
+        # Rigid transformation via SVD of covariance matrix
+        u, s, vh = np.linalg.svd(np.dot(v1, v0.T))
+        # rotation matrix from SVD orthonormal bases
+        R = np.dot(u, vh)
+        if np.linalg.det(R) < 0.0:
+            # R does not constitute right handed system
+            R -= np.outer(u[:, ndims-1], vh[ndims-1, :]*2.0)
+            s[-1] *= -1.0
+        # homogeneous transformation matrix
+        M = np.identity(ndims+1)
+        M[:ndims, :ndims] = R
+    else:
+        # Rigid transformation matrix via quaternion
+        # compute symmetric matrix N
+        xx, yy, zz = np.sum(v0 * v1, axis=1)
+        xy, yz, zx = np.sum(v0 * np.roll(v1, -1, axis=0), axis=1)
+        xz, yx, zy = np.sum(v0 * np.roll(v1, -2, axis=0), axis=1)
+        N = [[xx+yy+zz, 0.0,      0.0,      0.0],
+             [yz-zy,    xx-yy-zz, 0.0,      0.0],
+             [zx-xz,    xy+yx,    yy-xx-zz, 0.0],
+             [xy-yx,    zx+xz,    yz+zy,    zz-xx-yy]]
+        # quaternion: eigenvector corresponding to most positive eigenvalue
+        w, V = np.linalg.eigh(N)
+        q = V[:, np.argmax(w)]
+        q /= vector_norm(q)  # unit quaternion
+        # homogeneous transformation matrix
+        M = quaternion_matrix(q)
+
+    if scale and not shear:
+        # Affine transformation; scale is ratio of RMS deviations from centroid
+        v0 *= v0
+        v1 *= v1
+        M[:ndims, :ndims] *= math.sqrt(np.sum(v1) / np.sum(v0))
+
+    # move centroids back
+    M = np.dot(np.linalg.inv(M1), np.dot(M, M0))
+    M /= M[ndims, ndims]
+    return M
+
+
 #Preferences
 class AlignmentAddonPreferences(AddonPreferences):
     # this must match the addon name, use '__package__'
@@ -118,7 +299,7 @@ class AlignmentAddonPreferences(AddonPreferences):
             description = "Number of iterations between redraw, bigger = less redraw but faster completion",
             default=10)
     
-    use_sampe = BoolProperty(
+    use_sample = BoolProperty(
             name = "Use Sample",
             description = "Use a sample of verts to align",
             default = False)
@@ -136,6 +317,18 @@ class AlignmentAddonPreferences(AddonPreferences):
             default = 0.5,
             min = 0,
             max = 20)
+    
+    target_d = FloatProperty(
+            name="Target Translation",
+            description = "If translation of 3 iterations is < target, ICP is considered sucessful",
+            default = 0.01,
+            min = 0,
+            max = 10)
+    
+    use_target = BoolProperty(
+            name="Use Target",
+            description = "Calc alignment stats at each iteration to assess convergence. SLower per step, may result in less steps",
+            default = True)
 
     def draw(self, context):
         layout = self.layout
@@ -145,6 +338,8 @@ class AlignmentAddonPreferences(AddonPreferences):
         layout.prop(self, "use_sample")
         layout.prop(self, "sample_fraction")
         layout.prop(self, "min_start")
+        layout.prop(self, "use_target")
+        layout.prop(self, "target_d")
         
 
 class ComplexAlignmentPanel(bpy.types.Panel):
@@ -345,7 +540,7 @@ class OJECT_OT_align_exclude_clear(bpy.types.Operator):
         return {'FINISHED'}
     
 
-def make_pairs(align_obj, base_obj, vlist, thresh, sample = 0):
+def make_pairs(align_obj, base_obj, vlist, thresh, sample = 0, calc_stats = False):
     '''
     vlist is a list of vertex indices in the align object to use
     for alignment
@@ -358,6 +553,8 @@ def make_pairs(align_obj, base_obj, vlist, thresh, sample = 0):
     
     verts1 = []
     verts2 = []
+    if calc_stats:
+        dists = []
     
     #downsample if needed
     if sample > 1:
@@ -380,6 +577,8 @@ def make_pairs(align_obj, base_obj, vlist, thresh, sample = 0):
             if face_index != -1 and dist < thresh:
                 verts1.append(mx1 * vert.co)
                 verts2.append(mx2 * co1)
+                if calc_stats:
+                    dists.append(dist)
         
         #later we will pre-process data to get nice data sets
         #eg...closest points after initial guess within a certain threshold
@@ -393,8 +592,14 @@ def make_pairs(align_obj, base_obj, vlist, thresh, sample = 0):
     
             A[i][0], A[i][1], A[i][2] = V1[0], V1[1], V1[2]
             B[i][0], B[i][1], B[i][2] = V2[0], V2[1], V2[2]
-            
-        return A, B
+        
+        if calc_stats:
+            avg_dist = np.mean(dists)
+            dev = np.std(dists)
+            d_stats = [avg_dist, dev]
+        else:
+            d_stats = None
+        return A, B, d_stats
         
 
 def draw_callback_px(self, context):
@@ -574,6 +779,10 @@ class OBJECT_OT_align_pick_points(bpy.types.Operator):
                 self.de_localize(context)
                 self.align_obj(context)
                 
+                context.scene.objects.active = self.obj_align
+                self.obj_align.select = True
+                self.obj_base = True
+                
                 return {'FINISHED'}
             
         return {'RUNNING_MODAL'}
@@ -628,14 +837,16 @@ class OBJECT_OT_align_pick_points(bpy.types.Operator):
         alt_trans = b_com - a_com
         print(alt_trans.length)
         
-        self.obj_align.location += trans
-        
-        self.obj_align.update_tag()
-        context.scene.update()
-        
+           
         quat = rot.to_quaternion()
+        
+        self.obj_align.location += self.obj_base.location + trans - quat * self.obj_align.matrix_world.to_translation()
+        
+        
         self.obj_align.rotation_mode = 'QUATERNION'
         self.obj_align.rotation_quaternion *= quat
+        
+        
         
         self.obj_align.update_tag()
         context.scene.update()
@@ -727,6 +938,7 @@ class OJECT_OT_icp_align(bpy.types.Operator):
     """Uses ICP alignment to iteratevely aligne two objects"""
     bl_idname = "object.align_icp"
     bl_label = "ICP Align"
+    bl_options = {'REGISTER', 'UNDO'}
 
     @classmethod
     def poll(cls, context):
@@ -770,11 +982,19 @@ class OJECT_OT_icp_align(bpy.types.Operator):
         thresh = context.user_preferences.addons['object_alignment'].preferences.min_start
         sample = context.user_preferences.addons['object_alignment'].preferences.sample_fraction
         iters = context.user_preferences.addons['object_alignment'].preferences.icp_iterations
+        target_d = context.user_preferences.addons['object_alignment'].preferences.target_d
+        use_target = context.user_preferences.addons['object_alignment'].preferences.use_target
         factor = round(1/sample)
         
+        n = 0
+        converged = False
+        conv_t_list = [target_d * 2] * 5  #store last 5 translations
+        conv_r_list = [None] * 5
         
-        for n in range(iters):
-            (A, B) = make_pairs(align_obj, base_obj, vlist, thresh, factor)
+        while n < iters  and not converged:
+            
+            
+            (A, B, d_stats) = make_pairs(align_obj, base_obj, vlist, thresh, factor, calc_stats = use_target)
             (R, T) = rigid_transform_3D(np.mat(A), np.mat(B))
             
             rot = Matrix(np.array(R))
@@ -785,7 +1005,30 @@ class OJECT_OT_icp_align(bpy.types.Operator):
             align_obj.update_tag()
             context.scene.update()
         
-        time_taken = time.time() - start 
+            if d_stats:
+                i = fmod(n,5)
+                conv_t_list[i] = trans.length
+                conv_r_list[i] = abs(quat.angle)
+                
+                if all(d < target_d for d in conv_t_list):
+                    converged = True
+                    
+                    
+                    print('Converged in %s iterations' % str(n+1))
+                    print('Final Translation: %f ' % conv_t_list[i])
+                    print('Final Avg Dist: %f' % d_stats[0])
+                    print('Final St Dev %f' % d_stats[1])
+                    print('Avg last 5 rotation angle: %f' % np.mean(conv_r_list))
+            
+            n += 1   
+        time_taken = time.time() - start
+        if use_target and not converged:
+            print('Maxed out iterations')
+            print('Final Translation: %f ' % conv_t_list[i])
+            print('Final Avg Dist: %f' % d_stats[0])
+            print('Final St Dev %f' % d_stats[1])
+            print('Avg last 5 rotation angle: %f' % np.mean(conv_r_list))
+            
         print('Aligned obj in %f sec' % time_taken)   
         return {'FINISHED'}
     
