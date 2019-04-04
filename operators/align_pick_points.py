patmo141/object_alignment

commit ab52a2da09e9dc5767f1e4482c2c1b3fe98772d6

Points picker now utilizes submodule

operators/align_pick_points.py
@@ -23,287 +23,91 @@ import numpy as np
 import bpy
 import blf
 from bpy.types import Operator
-from mathutils import Matrix
+from mathutils import Vector, Matrix
 from bpy_extras import view3d_utils
 
 # Addon imports
 from ..functions import *
+from ..lib.points_picker.operators.points_picker import *
 
 
-def draw_callback_px(self, context):
-
-    font_id = 0  # XXX, need to find out how best to get this.
-
-    # draw some text
-    y = context.region.height
-    dims = blf.dimensions(0, 'A')
-
-    blf.position(font_id, 10, y - 20 - dims[1], 0)
-    blf.size(font_id, 20, 72)
-
-    if context.area.x == self.area_align.x:
-        blf.draw(font_id, "Align: "+ self.align_msg)
-        points = [self.obj_align.matrix_world * p for p in self.align_points]
-        color = (1,0,0,1)
-    else:
-        blf.draw(font_id, "Base: " + self.base_msg)
-        points = [self.obj_align.matrix_world * p for p in self.base_points]
-        color = (0,1,0,1)
-
-    draw_3d_points_revised(context, points, color, 4)
-
-    for i, vec in enumerate(points):
-        ind = str(i)
-        draw_3d_text(context, font_id, ind, vec)
-
-class OBJECT_OT_align_pick_points(Operator):
+class OBJECT_OT_align_pick_points(VIEW3D_OT_points_picker):
     """Align two objects with 3 or more pair of picked points"""
     bl_idname = "object.align_picked_points"
     bl_label = "Align: Picked Points"
     # bl_options = {"REGISTER", "UNDO"}
 
-    ################################################
-    # Blender Operator methods
+    #############################################
+    # overwriting functions from Points Picker submodule
 
     @classmethod
-    def poll(cls, context):
+    def can_start(cls, context):
+        """ Start only if editing a mesh """
+        ob = context.active_object
         condition_1 = len(context.selected_objects) == 2
-        condition_2 = context.object.type == 'MESH'
+        condition_2 = ob and ob.type == 'MESH'
         return condition_1 and condition_2
 
-    def modal(self, context, event):
+    def start_post(self):
+        self.snap_type = "SCENE"
+        self.align_points = list()
+        self.base_points = list()
+        align_obj_name = bpy.context.object.name
+        base_obj_name  = [obj for obj in bpy.context.selected_objects if obj != bpy.context.object][0].name
+        self.align_obj = bpy.data.objects[align_obj_name]
+        self.base_obj = bpy.data.objects[base_obj_name]
 
-        tag_redraw_areas("VIEW_3D")
-
-        if len(self.align_points) < 3:
-            self.align_msg = "Pick at least %s more pts" % str(3 - len(self.align_points))
+    def add_point_post(self, point):
+        if point.source_object == self.align_obj:
+            self.align_points.append(point)
+        elif point.source_object == self.base_obj:
+            self.base_points.append(point)
         else:
-            self.align_msg = "More points optional"
+            self.b_pts.pop(self.selected)
 
-        if len(self.base_points) < 3:
-            self.base_msg = "Pick at last %s more pts" % str(3 - len(self.base_points))
-        else:
-            self.base_msg = "More points optional"
+    def end_commit(self):
+        """ Commit changes to mesh! """
+        scn = bpy.context.scene
+        for pt in self.b_pts:
+            # self.de_localize(context)
+            self.align_objects(bpy.context)
+            select(self.align_obj, active=True)
 
-
-        if len(self.base_points) > 3 and len(self.align_points) > 3 and len(self.base_points) != len(self.align_points):
-
-            if len(self.align_points) < len(self.base_points):
-                self.align_msg = "Pick %s more pts to match" % str(len(self.base_points) - len(self.align_points))
-            else:
-                self.base_msg = "Pick %s more pts to match" % str(len(self.align_points) - len(self.base_points))
-
-        if len(self.base_points) == len(self.align_points) and len(self.base_points) >= 3:
-            self.base_msg = "Hit Enter to Align"
-            self.align_msg = "Hit Enter to Align"
-
-
-        if event.type == 'LEFTMOUSE' and event.value == 'PRESS':
-
-            ray_max = 10000
-
-            if event.mouse_x > self.area_align.x and event.mouse_x < self.area_align.x + self.area_align.width:
-
-                for reg in self.area_align.regions:
-                    if reg.type == 'WINDOW':
-                        region = reg
-                for spc in self.area_align.spaces:
-                    if spc.type == 'VIEW_3D':
-                        rv3d = spc.region_3d
-
-                #just transform the mouse window coords into the region coords
-                coord = (event.mouse_x - region.x, event.mouse_y - region.y)
-
-                #are the cords the problem
-                print('align cords: ' + str(coord))
-                print(str((event.mouse_region_x, event.mouse_region_y)))
-
-                view_vector = view3d_utils.region_2d_to_vector_3d(region, rv3d, coord)
-                ray_origin = view3d_utils.region_2d_to_origin_3d(region, rv3d, coord)
-                ray_target = ray_origin + (view_vector * ray_max)
-
-                print('in the align object window')
-                (d, (ok,hit, normal, face_index)) = ray_cast_region2d(region, rv3d, coord, self.obj_align)
-                if hit:
-                    print('hit! align_obj %s' % self.obj_align.name)
-                    #local space of align object
-                    self.align_points.append(hit)
-
-            else:
-
-                for reg in self.area_base.regions:
-                    if reg.type == 'WINDOW':
-                        region = reg
-                for spc in self.area_base.spaces:
-                    if spc.type == 'VIEW_3D':
-                        rv3d = spc.region_3d
-
-                coord = (event.mouse_x - region.x, event.mouse_y - region.y)
-                view_vector = view3d_utils.region_2d_to_vector_3d(region, rv3d, coord)
-                ray_origin = view3d_utils.region_2d_to_origin_3d(region, rv3d, coord)
-                ray_target = ray_origin + (view_vector * ray_max)
-
-                print('in the base object window')
-                (d, (ok,hit, normal, face_index)) = ray_cast_region2d(region, rv3d, coord, self.obj_base)
-                if ok:
-                    print('hit! base_obj %s' % self.obj_base.name)
-                    #points in local space of align object
-                    self.base_points.append(self.obj_align.matrix_world.inverted() * self.obj_base.matrix_world * hit)
-
-
-            return {'RUNNING_MODAL'}
-
-        elif event.type == 'RIGHTMOUSE' and event.value == 'PRESS':
-
-            if event.mouse_x > self.area_align.x and event.mouse_x < self.area_align.x + self.area_align.width:
-                self.align_points.pop()
-            else:
-                self.base_points.pop()
-
-            return {'RUNNING_MODAL'}
-
-
-        if event.type in {'WHEELUPMOUSE', 'WHEELDOWNMOUSE'}:
-
-            return {'PASS_THROUGH'}
-
-        if self.modal_state == 'NAVIGATING':
-
-            if (event.type in {'MOUSEMOVE',
-                               'MIDDLEMOUSE',
-                                'NUMPAD_2',
-                                'NUMPAD_4',
-                                'NUMPAD_6',
-                                'NUMPAD_8',
-                                'NUMPAD_1',
-                                'NUMPAD_3',
-                                'NUMPAD_5',
-                                'NUMPAD_7',
-                                'NUMPAD_9'} and event.value == 'RELEASE'):
-
-                self.modal_state = 'WAITING'
-                return {'PASS_THROUGH'}
-
-
-        if (event.type in {'MIDDLEMOUSE',
-                                    'NUMPAD_2',
-                                    'NUMPAD_4',
-                                    'NUMPAD_6',
-                                    'NUMPAD_8',
-                                    'NUMPAD_1',
-                                    'NUMPAD_3',
-                                    'NUMPAD_5',
-                                    'NUMPAD_7',
-                                    'NUMPAD_9'} and event.value == 'PRESS'):
-
-            self.modal_state = 'NAVIGATING'
-
-            return {'PASS_THROUGH'}
-
-        elif event.type in {'ESC'}:
-            bpy.types.SpaceView3D.draw_handler_remove(self._handle, 'WINDOW')
-            return {'CANCELLED'}
-
-        elif event.type == 'RET':
-
-            if len(self.align_points) >= 3 and len(self.base_points) >= 3 and len(self.align_points) == len(self.base_points):
-                bpy.types.SpaceView3D.draw_handler_remove(self._handle, 'WINDOW')
-                self.de_localize(context)
-                self.align_obj(context)
-
-                context.scene.objects.active = self.obj_align
-                self.obj_align.select = True
-                self.obj_base = True
-
-                return {'FINISHED'}
-
-        return {'RUNNING_MODAL'}
-
-    def invoke(self, context, event):
-        self.modal_state = 'WAITING'
-
-        self.start_time = time.time()
-        #capture some mouse info to pass to the draw handler
-        self.winx = event.mouse_x
-        self.winy = event.mouse_y
-
-        self.regx = event.mouse_region_x
-        self.regy = event.mouse_region_y
-
-        self.base_msg = 'Select 3 or more points'
-        self.align_msg = 'Select 3 or more points'
-
-
-        obj1_name = context.object.name
-        obj2_name = [obj for obj in context.selected_objects if obj != context.object][0].name
-
-        for ob in context.scene.objects:
-            ob.select = False
-
-        context.scene.objects.active = None
-
-        #I did this stupid method becuase I was unsure
-        #if some things were being "sticky" and not
-        #remembering where they were
-        obj1 = bpy.data.objects[obj1_name]
-        obj2 = bpy.data.objects[obj2_name]
-
-        for ob in bpy.data.objects:
-            if ob.select:
-                print(ob.name)
-
-        screen = context.window.screen
-        areas = [area.as_pointer() for area in screen.areas]
-        for area in screen.areas:
-            if area.type == 'VIEW_3D':
-                break
-
-        bpy.ops.view3d.toolshelf() #close the first toolshelf
-        override = context.copy()
-        override['area'] = area
-
-        self.area_align = area
-
-        bpy.ops.screen.area_split(override, direction='VERTICAL', factor=0.5, mouse_x=-100, mouse_y=-100)
-        #bpy.ops.view3d.toolshelf() #close the 2nd toolshelf
-
-        context.scene.objects.active = obj1
-        obj1.select = True
-        obj2.select = False
-
-        bpy.ops.view3d.localview(override)
-
-        obj1.select = False
-        context.scene.objects.active = None
-        override = context.copy()
-        for area in screen.areas:
-            if area.as_pointer() not in areas:
-                override['area'] = area
-                self.area_base = area
-                bpy.ops.object.select_all(action = 'DESELECT')
-                context.scene.objects.active = obj2
-                obj2.select = True
-                override['selected_objects'] = [obj2]
-                override['selected_editable_objects'] = [obj2]
-                override['object'] = obj2
-                override['active_object'] = obj2
-                bpy.ops.view3d.localview(override)
-                break
-
-
-        self.obj_align = obj1
-        self.obj_base = obj2
-
-        #hooray, we will raycast in local view!
-        self.align_points = []
-        self.base_points = []
-
-        context.window_manager.modal_handler_add(self)
-        self._handle = bpy.types.SpaceView3D.draw_handler_add(draw_callback_px, (self, context), 'WINDOW', 'POST_PIXEL')
-        return {'RUNNING_MODAL'}
+    def getLabel(self, idx):
+        point = self.b_pts[idx]
+        if point.source_object == self.align_obj:
+            return "A" + str(len(self.align_points))
+        elif point.source_object == self.base_obj:
+            return "B" + str(len(self.base_points))
 
     #############################################
-    # class methods
+    # additional functions
+
+    # def get_matrix_world_for_point(self, pt):
+    #    Z = pt.surface_normal
+    #    x_rand = Vector((random.random(), random.random(), random.random()))
+    #    x_rand.normalize()
+    #
+    #    if abs(x_rand.dot(Z)) > .9:
+    #        x_rand = Vector((random.random(), random.random(), random.random()))
+    #        x_rand.normalize()
+    #    X = x_rand - x_rand.dot(Z) * Z
+    #    X.normalize()
+    #
+    #    Y = Z.cross(X)
+    #
+    #    R = Matrix.Identity(3)  #make the columns of matrix U, V, W
+    #    R[0][0], R[0][1], R[0][2]  = X[0] ,Y[0],  Z[0]
+    #    R[1][0], R[1][1], R[1][2]  = X[1], Y[1],  Z[1]
+    #    R[2][0] ,R[2][1], R[2][2]  = X[2], Y[2],  Z[2]
+    #    R = R.to_4x4()
+    #
+    #    if pt.label == "Replacement Point":
+    #        T = Matrix.Translation(pt.location + 2 * Z)
+    #    else:
+    #        T = Matrix.Translation(pt.location)
+    #
+    #    return T * R
 
     def de_localize(self,context):
 
@@ -322,11 +126,12 @@ class OBJECT_OT_align_pick_points(Operator):
 
         #ret = bpy.ops.screen.area_join(min_x=area_base.x,min_y=area_base.y, max_x=area_align.x, max_y=area_align.y)
 
-    def align_obj(self,context):
+    def align_objects(self, context):
+        scn = bpy.context.scene
 
+        # match length of both lists to the shortest of the two
         if len(self.align_points) != len(self.base_points):
             if len(self.align_points) < len(self.base_points):
-
                 self.base_points = self.base_points[0:len(self.align_points)]
             else:
                 self.align_points = self.align_points[0:len(self.base_points)]
@@ -338,8 +143,8 @@ class OBJECT_OT_align_pick_points(Operator):
             V1 = self.align_points[i]
             V2 = self.base_points[i]
 
-            A[0][i], A[1][i], A[2][i] = V1[0], V1[1], V1[2]
-            B[0][i], B[1][i], B[2][i] = V2[0], V2[1], V2[2]
+            A[0][i], A[1][i], A[2][i] = V1.location.x, V1.location.y, V1.location.z
+            B[0][i], B[1][i], B[2][i] = V2.location.x, V2.location.y, V2.location.z
 
 
         #test new method
@@ -361,7 +166,8 @@ class OBJECT_OT_align_pick_points(Operator):
 
         #because we calced transform in local space
         #it's this easy to update the obj...
-        self.obj_align.matrix_world = self.obj_align.matrix_world * new_mat
+        self.align_obj.matrix_world = self.align_obj.matrix_world * new_mat
 
-        self.obj_align.update_tag()
-        context.scene.update()
+        self.align_obj.update_tag()
+        scn.update()
+    #############################################

.gitmodules
@@ -1,3 +1,6 @@
 [submodule "addon_common"]
 	path = addon_common
 	url = https://github.com/CGCookie/addon_common
+[submodule "lib/points_picker"]
+	path = lib/points_picker
+	url = https://github.com/bblanimation/points_picker

lib/__init__.py
@@ -15,6 +15,6 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-from .classesToRegister import *
+# from .classesToRegister import *
 from .preferences import *
 from .reportError import *

lib/classesToRegister.py
@@ -22,12 +22,12 @@ from ..operators import *
 
 classes = [
     ObjectAlignmentPreferences,
-    OBJECT_OT_icp_align,
-    OBJECT_OT_icp_align_feedback,
     OBJECT_OT_align_add_include,
     OBJECT_OT_align_add_exclude,
     OBJECT_OT_align_include_clear,
     OBJECT_OT_align_exclude_clear,
     OBJECT_OT_align_pick_points,
+    OBJECT_OT_icp_align_feedback,
+    OBJECT_OT_icp_align,
     VIEW3D_PT_object_alignment,
 ]

commit b0a7b3f5fa11aa1a1badd89920ff47d5633593fd

Syntax updates. Bug exists in align_objects
- bug in align_objects was introduced in commit ab52a2d

operators/align_pick_points.py
@@ -55,10 +55,8 @@ class OBJECT_OT_align_pick_points(VIEW3D_OT_points_picker):
         # set up custom data structures
         self.align_points = list()
         self.base_points = list()
-        align_obj_name = bpy.context.object.name
-        base_obj_name  = [obj for obj in bpy.context.selected_objects if obj != bpy.context.object][0].name
-        self.align_obj = bpy.data.objects[align_obj_name]
-        self.base_obj = bpy.data.objects[base_obj_name]
+        self.align_obj = bpy.context.active_object
+        self.base_obj = [obj for obj in bpy.context.selected_objects if obj != bpy.context.object][0]
 
         # additional UI changes
         # self.additional_ui_setup()
@@ -127,11 +125,11 @@ class OBJECT_OT_align_pick_points(VIEW3D_OT_points_picker):
         bpy.ops.view3d.localview(override)
         bpy.ops.view3d.view_selected(override)
 
-        #Crash Blender?
+        # Crash Blender?
         bpy.ops.screen.area_join(min_x=self.area_align.x,min_y=self.area_align.y, max_x=self.area_base.x, max_y=self.area_base.y)
         bpy.ops.view3d.toolshelf()
 
-        #ret = bpy.ops.screen.area_join(min_x=area_base.x,min_y=area_base.y, max_x=area_align.x, max_y=area_align.y)
+        # ret = bpy.ops.screen.area_join(min_x=area_base.x,min_y=area_base.y, max_x=area_align.x, max_y=area_align.y)
 
     def align_objects(self, context):
         scn = bpy.context.scene
@@ -143,10 +141,10 @@ class OBJECT_OT_align_pick_points(VIEW3D_OT_points_picker):
             else:
                 self.align_points = self.align_points[0:len(self.base_points)]
 
-        A = np.zeros(shape = [3,len(self.base_points)])
-        B = np.zeros(shape = [3,len(self.base_points)])
+        A = np.zeros(shape=[3, len(self.base_points)])
+        B = np.zeros(shape=[3, len(self.base_points)])
 
-        for i in range(0,len(self.base_points)):
+        for i in range(0, len(self.base_points)):
             V1 = self.align_points[i]
             V2 = self.base_points[i]
 
@@ -154,16 +152,15 @@ class OBJECT_OT_align_pick_points(VIEW3D_OT_points_picker):
             B[0][i], B[1][i], B[2][i] = V2.location.x, V2.location.y, V2.location.z
 
 
-        #test new method
-        settings = get_addon_preferences()
-        align_meth = settings.align_meth
+        # test new method
+        align_meth = get_addon_preferences().align_meth
 
         if align_meth == '0': #rigid transform
             M = affine_matrix_from_points(A, B, shear=False, scale=False, usesvd=True)
         elif align_meth == '1': # rot, loc, scale
             M = affine_matrix_from_points(A, B, shear=False, scale=True, usesvd=True)
-        #else: #affine
-            #M = affine_matrix_from_points(A, B, shear=True, scale=True, usesvd=True)
+        # else: #affine
+        #     M = affine_matrix_from_points(A, B, shear=True, scale=True, usesvd=True)
 
 
         new_mat = Matrix.Identity(4)
@@ -171,8 +168,8 @@ class OBJECT_OT_align_pick_points(VIEW3D_OT_points_picker):
             for m in range(0,4):
                 new_mat[n][m] = M[n][m]
 
-        #because we calced transform in local space
-        #it's this easy to update the obj...
+        # because we calced transform in local space
+        # it's this easy to update the obj...
         self.align_obj.matrix_world = self.align_obj.matrix_world * new_mat
 
         self.align_obj.update_tag()
@@ -185,7 +182,7 @@ class OBJECT_OT_align_pick_points(VIEW3D_OT_points_picker):
             if area.type == 'VIEW_3D':
                 break
 
-        bpy.ops.view3d.toolshelf() #close the first toolshelf
+        bpy.ops.view3d.toolshelf() # close the first toolshelf
         override = bpy.context.copy()
         override['area'] = area
 